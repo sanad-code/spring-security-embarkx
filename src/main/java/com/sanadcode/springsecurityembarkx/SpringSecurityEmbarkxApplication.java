3b37dc9b450819a9359c6efc75c38974b8a9b4da
Moataz Sanad <sanad.code.io@gmail.com>
2024-07-20 08:19:24 +0300

    first commit

---
+++ src/main/java/com/sanadcode/springsecurityembarkx/SpringSecurityEmbarkxApplication.java
@@ -0,0 +1,13 @@
+package com.sanadcode.springsecurityembarkx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityEmbarkxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityEmbarkxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanadcode/springsecurityembarkx/SpringSecurityEmbarkxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanadcode.springsecurityembarkx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityEmbarkxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
